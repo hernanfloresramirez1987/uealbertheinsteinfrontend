0fb803203f7fae9c5d514439000104e97fa937a9
HernanFloresRamirez@Hernan <hernanfloresramirez1987@gmail.com>
2024-04-27 03:10:04 -0400

    subiendo menus dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'login', loadComponent: () => import('./core/components/auth/login/login.component') },
   { path: 'autorizationrol', loadComponent: () => import('./core/components/authorization/authorization.component') },
+  { path: 'dashboard', loadComponent: () => import('./core/components/dashboard-main/dashboard-main.component') },
   { path: 'employes', loadChildren: () => import('./core/components/pages/employes/employes.module').then(m => m.EmployesModule) },
   { path: 'restorepassword', loadComponent: () => import('./core/components/auth/password-recovery/password-recovery.component') },
   { path: 'home', loadComponent: () => import('./home/home.component') },
